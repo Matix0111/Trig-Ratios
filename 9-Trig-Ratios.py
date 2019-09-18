c03be316a292258d9eb46383dee0741b983fa4b5
root <root@localhost.localdomain>
2019-09-18 11:17:17 -0400

    None

--- 9-Trig-Ratios.py
+++ 9-Trig-Ratios.py
@@ -1,2 +1,15 @@
 # This program is designed for trig ratios (SOH CAH TOA)
 import math
+
+class TrigFunc:
+    print("OPTIONS: SOH, CAH, TOA")
+    trig_ratio = input("Trig ratio: ")
+
+    if trig_ratio == 'SOH':
+        SOH()
+    elif trig_ratio == 'CAH':
+        pass
+    elif trig_ratio == 'TOA':
+        pass
+
+    def SOH():
