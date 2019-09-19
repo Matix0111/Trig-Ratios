f95eb4c11ef78b57a942df65b53687135e6232a6
Matix0111 <55466185+Matix0111@users.noreply.github.com>
2019-09-19 12:05:03 -0400

    Update 9-Trig-Ratios.py

--- 9-Trig-Ratios.py
+++ 9-Trig-Ratios.py
@@ -18,7 +18,7 @@ class TrigFunc:
             print("EXITING")
             return
         elif request == "C" or request == "c":
-            sin_frac = input("Is the Numerator(N/n) or Denominator(D/d) the VARIABLE: ")
+            sin_frac = input("Is the Numerator(N/n) or Denominator(D/d) the VARIABLE? ")
 
             if sin_frac == "N" or sin_frac == "n":
                 number = input("Number: ")
@@ -27,26 +27,39 @@ class TrigFunc:
                 answer_round = round(answer, 2)
                 print("Answer: {}".format(answer_round))
             elif sin_frac == "D" or sin_frac == "d":
-                print("THIS FUNCTION IS IN PROGRESS")
-                timer = 6
-                while True:
-                    time.sleep(1)
-                    timer -= 1
+                div_number = input("Number: ")
 
-                    print("Exiting in: {}".format(timer))
+                div_answer = int(div_number) / math.sin(math.radians(int(sin_degrees)))
 
-                    if timer > 0:
-                        pass
-                    elif timer <= 0:
-                        return
-                # div_number = input("Number: ")
+                print(div_answer)
 
-                # div_answer =
+    def CAH():
+        cos_deg = input("Degrees: ")
+        cos_rad = math.cos(math.radians(int(cos_deg)))
+        cos_round = round(cos_rad, 2)
+        print("RADIAN: {} ROUNDED: {}".format(cos_rad, cos_round))
 
-        def CAH():
-            pass
-        def TOA():
-            pass
+        request_cos = input("Exit(E/e) or Continue(C/c): ")
+        if request_cos == 'E' or request_cos == 'e':
+            print("EXITING")
+            return
+        elif request_cos == 'C' or request_cos == "c":
+            cos_frac = input("Is the Numerator(N/n) or Denominator(D/d) the VARIABLE? ")
+
+            if cos_frac == "N" or cos_frac == 'n':
+                cos_number = input("Number: ")
+
+                cos_answer = cos_rad * int(cos_number)
+                cos_answer_round = round(cos_answer, 2)
+                print("Answer: {}".format(cos_answer_round))
+
+            elif cos_frac == "D" or cos_frac == "d":
+                div_cos = input("Number: ")
+
+                div_cos_answer = div_cos / math.cos(math.radians(int(cos_deg)))
+                print("Answer: {}".format(div_cos_answer))
+    def TOA():
+        pass
 
 
     if trig_ratio == 'SOH':
@@ -55,5 +68,7 @@ class TrigFunc:
         CAH()
     elif trig_ratio == 'TOA':
         TOA()
+    else:
+        print("This is an invalid option, please relaunch program")
 
 TrigFunc()
